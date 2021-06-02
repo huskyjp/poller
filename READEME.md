fa23fb5c1d0e6ba7ecfdab540ec624b7346de074
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-06-02 02:54:36 -0400

    first commit

---
+++ READEME.md
@@ -0,0 +1,9 @@
+# Poller
+
+Tech
+- Typescript
+- React
+- Node.js
+- GraphQL
+- Next.js
+- PostgreSQL
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,18 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import {MikroORM} from "@mikro-orm/core";
+import path from "path";
+import { dirname } from "path/posix";
+
+export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d.[tj]s$/,
+  },
+  dbName: 'poller',
+  entities: [Post],
+  // user: 'poller',
+  password: '12120506',
+  type: 'postgresql',
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];  // to convert as exact type - Parameters type return array
