f8208672d1a0355bbad2473250182b9b1dc67b15
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-06-04 17:28:14 -0400

    implemented react register.tsx with Formik

---
+++ with-chakra-ui-app/src/pages/register.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { Button, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import { Wrapper } from "../components/Wrapper";
+import { InputField } from "../components/InputField";
+
+interface registerProps {}
+
+export const Register: React.FC<registerProps> = ({}) => {
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name="username"
+              placeholder="username"
+              label="Username"
+            />
+            <InputField
+              name="password"
+              placeholder="password"
+              label="password"
+            />
+            <Button mt={4} type="submit" isLoading={isSubmitting} color="teal">
+              Register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Register;
