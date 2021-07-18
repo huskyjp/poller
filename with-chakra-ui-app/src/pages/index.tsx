213eb040eb91698e31a6fef1a7b72945384ec85f
taishi <39787700+taiseaocean@users.noreply.github.com>
2021-07-18 17:26:01 +0900

    added usePostsQuery

--- with-chakra-ui-app/src/pages/index.tsx
+++ with-chakra-ui-app/src/pages/index.tsx
@@ -1,11 +1,20 @@
 import { NavBar } from "../components/NavBar";
 import { withUrqlClient } from "next-urql";
-import { createurqlClient } from "../utils/createUrqlClient";
+import { createUrqlClient } from "../utils/createUrqlClient";
+import { usePostsQuery } from "../generated/graphql";
 
-const Index = () => (
-  <>
-    <NavBar />
-    <div>Hello World Next.js</div>
-  </>
-);
-export default withUrqlClient(createurqlClient)(Index);
+const Index = () => {
+  const [{ data }] = usePostsQuery();
+  return (
+    <>
+      <NavBar />
+      <div>Hello World Next.js</div>
+      {!data ? (
+        <div>Loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </>
+  );
+};
+export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
